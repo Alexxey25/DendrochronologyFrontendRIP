c6861f3fcb0db194783cc214f806dd80e5cb2057
lexa <alexalexandrov2005@gmail.com>
2026-05-17 23:26:56 +0300

    tauriComplete

--- src/config/backendConstants.ts
+++ src/config/backendConstants.ts
@@ -1,3 +1,2 @@
-/** Реэкспорт для модулей приложения; из vite.config импортируйте только `./backendHost`. */
 export { apiOrigin, API_PORT, BACKEND_HOST, MINIO_PORT } from './backendHost'
 export { minioObjectUrl } from './minioPublicOrigin'
